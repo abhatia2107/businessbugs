6b95516c97cbd005fbb475432ac1810ebb213117
Shefali Agarwal <shefu.ag911@gmail.com>
2015-02-05 23:27:00 +0530

    Aboutus and home page created

---
+++ app/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.layoutdef')
+@section('contents')
+<div class="banner">
+	<img src="/assets/images/IMG_2266.JPG">
+	<section class="intro">
+		<div class="col12">
+			<button type="button" class="btn" id="buy">Buy It Now!<span class="glyphicon glyphicon-chevron-right"></span></button>
+		</div>
+	</section>
+	</img>
+</div>
+<section class="jumbotron description">
+	<p>We at Business Bugs commit to get you what is best for business. The right news/views, at the right platform at the right time will be our working direction. We will ensure that we thrive towards our motto of making everybody self-reliant & confident who is seeking entrepreneurship as the career choice.  Every month, our endeavor will be to give insights about the new business segments in the simplest way. From how to make a photography enthusiast become a professional photographer, to how to transform a content writer into a blogger; we will continue to engage the people around.
+
+Our belief is to bring a revolution in the entrepreneurship market. We aim to reach approximately 10 lakh readers in this year. We will continue till we make entrepreneurship as the new cool. 
+	</p>
+	</section>
+@endsection
