84db67c60a06962ab8298d28c503052d4cd53ea9
kdmeena <kdmeena@gmail.com>
2015-02-26 19:13:35 +0530

    Payment form modified

--- app/views/Payments/create.blade.php
+++ app/views/Payments/create.blade.php
@@ -1,5 +1,13 @@
 @extends('Layouts.layout')
 @section('content')
+<style type="text/css">
+label {
+    width:180px;
+    clear:left;
+    text-align:right;
+    padding-right:10px;
+}
+</style>
     <form action="{{$posted['action']}}" method="post" role="form" enctype="multipart/form-data" name="payuForm">
     <input type="hidden" name="key" value="{{$posted['key']}}">
     <input type="hidden" name="hash" value="{{$posted['hash']}}">
@@ -11,15 +19,48 @@
     <input type="hidden" name="curl" value="{{$posted['curl']}}">
     <input type="hidden" name="service_provider" value="{{$posted['service_provider']}}">
     <input type="hidden" name="productinfo" value="{{$posted['productinfo']}}">
-
-    <td>First Name: </td>
-    <input type="text" name="firstname" value="{{$posted['firstname']}}">
-    <td>Last Name: </td>
-    <input name="lastname" value="{{$posted['lastname']}}">
-    <td>Email: </td>
-    <input name="email" value="{{$posted['email']}}">
-    <td>Phone: </td>
-    <input name="phone" value="{{$posted['phone']}}">
-    <input type="submit" value="Submit" />
+    <div class="row">
+                        <div class="form-group required">
+                            <label for="first_name" class="col-sm-3 control-label ">First Name<sup>*</sup>
+                            </label>
+                            <div class="col-sm-4">
+                                <input type="text"   class="form-control " name="firstname"  id="firstname" value="{{$posted['firstname']}}">
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="last_name"  class="col-sm-3 control-label ">Last Name</label>
+                            <div class="col-sm-4">
+                                <input type="text"  class="form-control " name="lastname"  id="lastname" value="{{$posted['lastname']}}">
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="form-group required">
+                            <label for="email" class="col-sm-3 control-label ">Email<sup>*</sup>
+                            </label>
+                            <div class="col-sm-4">
+                                <input type="email"  placeholder="mymail@example.com" class="form-control " name="email"  id="email" value="{{$posted['email']}}">
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="form-group required">
+                            <label for="contact_no" class="col-sm-3 control-label ">Mobile Number<sup>*</sup></label>
+                            <div class="col-sm-4">
+                                <input type="tel" class="form-control " name="phone"  id="contact_no" value="{{$posted['phone']}}">
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="row">
+                        <div class="form-group required">
+                            <div class="col-sm-4">
+                            </div>
+                            <div class="col-sm-4">
+                    <button type="Submit"class="btn-success" >Looks Good</button>
+                    </div>
+                    </div>
+                    </div>
     </form>
 @stop
