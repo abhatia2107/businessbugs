105dba10db331fdabbed6389c72ba3dd694acb73
Shefali Agarwal <shefu.ag911@gmail.com>
2015-03-04 16:49:58 +0530

    success and failure pages made

--- app/views/Payments/failure.blade.php
+++ app/views/Payments/failure.blade.php
@@ -1,8 +1,16 @@
 @extends('Layouts.layout')
 @section('content')
-
-<h3>Your order status is {{ $status}} </h3>
-<h4>Your transaction id for this transaction is {{$txnid}}. You may try making the payment by clicking the link below.</h4>
+<div class="main-container payments wrapper">
+	<div class="panel panel-danger">
+		<div class="panel-heading text-center">Your order status is {{$status}}.</div>
+		<div class="panel-body">
+		<div class="lead text-center">
+<h4>Your transaction id for this transaction is {{$txnid}}. </h4>
+<h5>You may try making the payment by clicking the link below.</h5>
 <!--Please enter your website homepage URL -->
 <p><a href={{url('/payments/create/')}}/{{$udf1}}> Try Again</a></p>
+</div>
+</div>
+</div>
+</div>
 @stop
--- app/views/Payments/success.blade.php
+++ app/views/Payments/success.blade.php
@@ -1,7 +1,16 @@
 @extends('Layouts.layout')
 @section('content')
-	<h3>Thank You. Your order status is {{$status}}.</h3>
-	<h4>Your Transaction ID for this transaction is {{$txnid}}.</h4>
-	<h4>We have received a payment of ₹ {{$amount}} for your order.</h4>
-	<h4>Your copy of {{$productinfo}} have been mailed to you.</h4>
+<div class="main-container payments wrapper">
+	<div class="panel panel-success">
+		<div class="panel-heading text-center">Thank You! Your order status is {{$status}}.</div>
+		<div class="panel-body">
+		<div class="lead text-center">
+	
+	<h4>Your Transaction ID for this transaction is <strong>{{$txnid}}</strong>.</h4>
+	<h4>We have received a payment of <strong>₹ {{$amount}}</strong> for your order.</h4>
+	<h4>Your copy of <strong>{{$productinfo}}</strong> have been mailed to you.</h4>
+	</div>
+	</div>
+	</div>
+	</div>
 @stop
